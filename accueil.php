c49b185971e7defdbf398ddc2bace4bf0a9a9cb7
leane35 <78912316+leane35@users.noreply.github.com>
2021-02-17 08:57:56 +0100

    Create accueil.php

---
+++ accueil.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link href="style_loader.css" rel="stylesheet">
+</head>
+
+<body>
+    <h2>Bonjour bonjour</h2>
+    <div>Comment allez-vous?</div>
+
+</body>
+<?php echo "Hello it's me " ?>
+</html>
